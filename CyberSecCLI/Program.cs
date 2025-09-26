b74295c9e308f17f4b262a3a137bb92b9367c60a
Ali-coder-crypto <ali.privat92@gmail.com>
2025-09-26 10:47:19 +0200

    Fixed Program.cs (logic error) and completed these 2 classes

--- CyberSecCLI/Program.cs
+++ CyberSecCLI/Program.cs
@@ -8,8 +8,12 @@ class Program
         {
             ShowMenu();
             var cipher = Console.ReadLine()?.Trim();
+            
             switch (cipher)
             {
+                case "0":
+                    Exit();
+                    break;
                 case "1":
                     CaesarCipherFunction("");
                     break;
@@ -17,12 +21,11 @@ class Program
                     Rot13CipherFunction("");
                     break;
             }
-
             continue;
 
             static void CaesarCipherFunction(string text)
             {
-               Console.WriteLine("Type 'encrypt' to encrypt or 'decrypt' to decrypt or.");
+               Console.WriteLine("Type 'encrypt' to encrypt or 'decrypt' to decrypt or 0 to exit.");
                var input = Console.ReadLine();
 
                if (input == null) return;
@@ -33,15 +36,19 @@ class Program
                        Console.Write("Enter text to encrypt: ");
                        var inputText = Console.ReadLine();
                        var encrypt = CeasarCipher.Encrypt(inputText);
-                       Console.WriteLine("Encrypt text: " + encrypt);
+                       Console.WriteLine("Encrypt text: " + "** " + encrypt + " **");
+                       var decrypt = CeasarCipher.Decrypt(encrypt);
+                       Console.WriteLine("Decrypt text: " + "** " + decrypt + " **");
                        break;
                    }
                    case "decrypt":
                    {
                        Console.Write("Enter text to decrypt: ");
                        var inputDecrypt = Console.ReadLine();
-                       var decrypt = CeasarCipher.Decrypt(inputDecrypt);
-                       Console.WriteLine("Decrypt text " + decrypt);
+                       var encrypt = CeasarCipher.Encrypt(inputDecrypt);
+                       Console.WriteLine("Decrypt text: " + "** " + encrypt +  " **");
+                       var decryptCaesar = CeasarCipher.Decrypt(encrypt);
+                       Console.WriteLine("Encrypt text: " +  "** " + decryptCaesar + " **");
                        break;
                    }
                }
@@ -50,7 +57,7 @@ class Program
 
             static void Rot13CipherFunction(string text)
             {
-                Console.WriteLine("Type 'encrypt' to encrypt or 'decrypt' to decrypt or.");
+                Console.WriteLine("Type 'encrypt' to encrypt or 'decrypt' to decrypt or 0 to exit.");
                 var input = Console.ReadLine();
 
                 if (input == null) return;
@@ -60,16 +67,21 @@ class Program
                    case "encrypt":
                        Console.Write("Enter text to encrypt: ");
                        var inputText = Console.ReadLine();
-                       var back = Rot13.EncryptRot13(inputText);
-                       Console.WriteLine("Encrypt text: " + back);
+                       var encryptRot13 = Rot13.EncryptRot13(inputText);
+                       Console.WriteLine("Encrypt text: " + "** " +  encryptRot13 + " **");
+                       var decryptRot13 = Rot13.EncryptRot13(encryptRot13);
+                       Console.WriteLine("Decrypt text: " + "** " + decryptRot13 + " **");
                        break;
                    case "decrypt":
                        Console.Write("Enter text to encrypt: ");
                        var inputDecrypt = Console.ReadLine();
-                       var backToNormal = Rot13.EncryptRot13(inputDecrypt);
-                       Console.WriteLine("Encrypt text: " + backToNormal);
+                       var decrypt = Rot13.DecryptRot13(inputDecrypt);
+                       Console.WriteLine("Encrypt text: " + "** " + decrypt + " **");
+                       var encrypt =  Rot13.EncryptRot13(inputDecrypt);
+                       Console.WriteLine("Decrypt text: " + "** " + encrypt + " **");
                        break;
                 }
+                Console.WriteLine(text);
             }
 
             static void ShowMenu()
@@ -78,6 +90,12 @@ class Program
                 Console.WriteLine("==Cryptography Menu==");
                 Console.WriteLine("1. Caesar Cipher");
                 Console.WriteLine("2. Rot13 Cipher");
+                Console.WriteLine("0. Exit");
+            }
+
+            static void Exit()
+            {
+                Environment.Exit(0);
             }
         }
     }
